254a1e1cbbc55f371e3cb42db5fd1be6248ad35d
natalia bilic <bilic.arq@gmail.com>
2024-06-17 19:55:26 -0300

    js naty

---
+++ js/SostenibilidadEmpresarial.js
@@ -0,0 +1,31 @@
+let nombreCompleto= prompt(`Queremos saber tu opinion sobre SOSTENIBILIDAD EN RIO GRANDE, por favor dejanos tu datos a continuacion.    
+     ¿Cual es tu nombre completo?`);
+
+if (nombreCompleto=="") {alert (`No ingresaste tu Nombre Completo`);} nombreCompleto= prompt(`Queremos saber tu opinion sobre SOSTENIBILIDAD EN RIO GRANDE, por favor dejanos tu datos a continuacion.    
+     ¿Cual es tu nombre completo?`);
+
+console.log (nombreCompleto);
+
+if ("let "=="") {alert (`Por favor completar`);}
+
+let Nacionalidad= prompt(`Cuentanos..  
+     ¿De que Nacionalidad eres?`)
+console.log (Nacionalidad);
+
+let Ciudad= prompt (`¿De qué Ciudad?`)
+console.log (Ciudad);
+
+let Comentarios= prompt (`Cuentanos que opinas al respecto,
+     ¿Crées que se puede mejorar y lograr tener una ciudad Sostenible?`)
+console.log (Comentarios);
+if (Comentarios=="si") {alert (`Que Alegria, nosotros tambien creemos que se puede`);} else {alert (`Esperamos que se logren grandes cambios, cada aporte es importante`);}
+
+
+let Comentarios2= prompt (`Tu opinión e ideas nos importan,
+    ¿Qué crees que se puede mejorar y cómo?`)
+console.log (Comentarios2);
+
+alert (`Nos visitó ${nombreCompleto}, de Nacionalidad ${Nacionalidad}, de la ciudad de ${Ciudad}, y nos dejo su importantisima opinión para mejorar la ciudad de Rio Grande:
+    ${Comentarios2}.`);
+
+alert (`${nombreCompleto} ¡Gracias por visitarnos y dejarnos tus comentarios! ❤️ `);
